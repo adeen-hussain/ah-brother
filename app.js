d30fc1a87851ddee44f64271dc026316a4639b0a
adeen-hussain <adeengamer972@gmail.com>
2025-10-06 02:24:31 +0500

    partial setup

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express();
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+// Routes
+app.get("/", (req, res) => res.render("index"));
+app.get("/about", (req, res) => res.render("about"));
+
+app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
